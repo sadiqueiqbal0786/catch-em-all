fa310bd25053bdcf752a3e8cb7eba245d0e46752
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-02-05 19:48:36 +0530

    done

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,6 +1,7 @@
 package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CatchEmAll {
 
@@ -12,6 +13,28 @@ public class CatchEmAll {
     }
 
     public static void main(String[] args) throws Exception {
-        riskyMethod();
-    }
-}
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("Resource is missing", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Resource error", e);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            throw e;
+        }
+    }}
+/*
+* Explanation:
+
+The main method calls the riskyMethod which can throw various types of exceptions.
+
+The first catch block handles IOException, and it wraps it with IllegalArgumentException and throws it.
+
+The second catch block handles FileNotFoundException, and it wraps it with IllegalArgumentException and throws it.
+
+The third catch block handles ArithmeticException and NumberFormatException, it just prints the error message to System.err but doesn't throw any exception.
+
+The fourth catch block is the catch-all block, it just throws the exception as it is.
+* */
